105b7b3025cce717d3465f27629d485dbc93d4fc
jalaverdiyeva <93456693+jalaverdiyeva@users.noreply.github.com>
2025-11-22 12:35:20 +0400

    InstagramFilterAI.java

--- src/InstagramFilterAI.java
+++ src/InstagramFilterAI.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class InstagramFilterAI {
+    public static void main(String[] args) {
+        int brightness=10;int contrast=53;
+        String filterName="Doja Dod";
+        boolean isPremium=true;
+
+        System.out.println("Initial Contrast: "+contrast);
+
+        if (isPremium) {
+            System.out.println("Premium filter detected. Applying enhancement...");
+            contrast+=20;
+        }
+
+        System.out.println("\n--- Final Filter Details ---");
+        System.out.println("Applied Filter: "+filterName);
+        System.out.println("Brightness Level: "+brightness);
+        System.out.println("Contrast Level: "+contrast);
+        System.out.println("Is Premium User?: "+isPremium);
+    }
 }
